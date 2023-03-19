9ddd054c72639a0368c035020bf1735d5fc624ef
Lab <Lab@HW-165737>
2023-03-19 23:09:24 +0200

    Canceled not relevative RP set.

--- UpdateFF2SpeakerMap.m
+++ UpdateFF2SpeakerMap.m
@@ -2,8 +2,8 @@ function UpdateFF2SpeakerMap( FFNumber, speakerNumber )
 global RP;
 load mdb; 
 mdb.Calibration.FF2SpeakerMap(FFNumber)  = speakerNumber;
-RP.SetTagVal(strcat('Gain',num2str(FFNumber)),...
-           10^( mdb.Calibration.SpeakersGain(speakerNumber)/20) );
+% RP.SetTagVal(strcat('Gain',num2str(FFNumber)),...
+%            10^( mdb.Calibration.SpeakersGain(speakerNumber)/20) );
 save mdb mdb;
 
 end
