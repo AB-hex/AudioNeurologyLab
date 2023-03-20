d53734eec4c13750c00fe06f613844a296391f48
Lab <Lab@HW-165737>
2023-03-20 14:38:40 +0200

    Canceled mdb writes, to implement new logic of writing to mdb only when going to play.

--- OptionsMenuOpenerHelper.m
+++ OptionsMenuOpenerHelper.m
@@ -1,10 +1,11 @@
 function   OptionsMenuOpenerHelper(app,TXNumberDouble)
        switch app.(strcat("InputSignal",num2str(TXNumberDouble),"TX",num2str(TXNumberDouble),"DropDown")).Value
        case 'Pure Tune'
-         PureTuneOptions(TXNumberDouble);
+        appOptions = PureTuneOptions(TXNumberDouble);
        case 'Noise'
-         NoiseOptionsApp(TXNumberDouble);
+         appOptions = NoiseOptionsApp(TXNumberDouble);
        case 'From File'
-         FileOptions(TXNumberDouble);
+         appOptions = FileOptions(TXNumberDouble);
        end
+        app.("TX"+TXNumberDouble+"Options") = appOptions;
 end
