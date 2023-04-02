5b3e185f6083f8ead2c04ad02d1d2c631ceabcea
Lab <Lab@HW-165737>
2023-04-02 09:28:24 +0300

    Fixed a missing load mdb line and arguments

--- SNRFinderPlayWord.m
+++ SNRFinderPlayWord.m
@@ -1,10 +1,11 @@
           
-function msg = SNRFinderPlayWord(app,wordsDir,stats,Idx,ii,NumOfWords,playOrder)
+function msg = SNRFinderPlayWord(app,signalLevel,noiseLevel,wordsDir,stats,Idx,ii,NumOfWords,playOrder)
 
            if(Idx.currentPlay>length(playOrder))
                 uialert(app.UIFigure,'All Words had been played.','Out of words');
                 return;
            end
+           load mdb
             name = wordsDir(playOrder(Idx.currentPlay)).name;
             filename = fullfile(wordsDir(playOrder(Idx.currentPlay)).folder,name);
             msg = stats+newline+ "Playing "+ii+" word out of "+NumOfWords+newline+name;
@@ -15,7 +16,7 @@ function msg = SNRFinderPlayWord(app,wordsDir,stats,Idx,ii,NumOfWords,playOrder)
             mdb.TX1.stimulus.speech.source = filename;
             mdb.TX1.stimulus.burstDuration = durationInSec;
             mdb.TX2.stimulus.burstDuration = durationInSec;
-            mdb.TX1.stimulus.speech.amp = signalLevel;
+            mdb.TX1.stimulus.speech.amp = signalLevel; 
             mdb.TX2.stimulus.noise.amp = noiseLevel;
             save mdb mdb;
             [~,~,~] = play_signal_multi(mdb.master.TX1_select,mdb.master.TX2_select,mdb.master.TX3_select); 
